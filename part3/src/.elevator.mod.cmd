66306cd3502861eac1a1feb0269745f20f137ab6
akelpie <jdmasopeh@outlook.com>
2023-11-08 09:17:08 -0500

    seperated files for organization

--- part3/src/.elevator.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/masopeh/project-2/elevator/part3/src/elevator.mod := printf '%s\n'   elevator.o | awk '!x[$$0]++ { print("/home/masopeh/project-2/elevator/part3/src/"$$0) }' > /home/masopeh/project-2/elevator/part3/src/elevator.mod
